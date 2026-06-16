6de87829f9ca3c996a6fe18cfab85d6fc77fb9a6
Praise <Praisewoherem@gmail.com>
2026-06-16 07:48:44 +0100

    Update QR scanner to OpenCV implementation

--- qr_scanner.py
+++ qr_scanner.py
@@ -1,16 +1,11 @@
 import cv2
-from pyzbar.pyzbar import decode
+import numpy as np
 
-def read_qr(image_path):
-    img = cv2.imread(image_path)
+def decode_qr(image_file):
+    file_bytes = np.asarray(bytearray(image_file.read()), dtype=np.uint8)
+    img = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
 
-    if img is None:
-        return None
+    detector = cv2.QRCodeDetector()
+    data, bbox, _ = detector.detectAndDecode(img)
 
-    decoded_objects = decode(img)
-
-    if decoded_objects:
-        qr_data = decoded_objects[0].data.decode("utf-8")
-        return qr_data
-
-    return None
+    return data
