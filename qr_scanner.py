c2d723ede67b4546f0acccd9ed6991997d3c6211
Praise <Praisewoherem@gmail.com>
2026-06-16 08:15:00 +0100

    Fix read_qr function in qr_scanner

--- qr_scanner.py
+++ qr_scanner.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-def decode_qr(image_file):
+def read_qr(image_file):
     file_bytes = np.asarray(bytearray(image_file.read()), dtype=np.uint8)
     img = cv2.imdecode(file_bytes, cv2.IMREAD_COLOR)
 
